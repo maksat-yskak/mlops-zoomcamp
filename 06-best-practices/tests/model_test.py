bd02ffbbefdc0c88ae3ba10ce25e2569dcf2e8ec
Maksat Yskak <maqsat.ysqaq@gmail.com>
2025-10-26 20:45:18 +0000

    Week 06: unit tests

---
+++ 06-best-practices/tests/model_test.py
@@ -0,0 +1,83 @@
+import model
+
+def test_base64_decode():
+    base64_input = "ewogICAgICAgICJyaWRlIjogewogICAgICAgICAgICAiUFVMb2NhdGlvbklEIjogMTMwLAogICAgICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICAgICAidHJpcF9kaXN0YW5jZSI6IDMuNjYKICAgICAgICB9LCAKICAgICAgICAicmlkZV9pZCI6IDE1NgogICAgfQ=="
+    actual_result =model.base64_decode(base64_input)
+    expected_result = {
+        "ride": {
+            "PULocationID": 130,
+            "DOLocationID": 205,
+            "trip_distance": 3.66
+        },
+        "ride_id": 156
+    }
+
+    assert actual_result == expected_result
+
+def test_prepare_features():
+    model_service = model.ModelService(None)  # model is not needed for this test
+
+    ride = {
+        'PULocationID': 10,
+        'DOLocationID': 20,
+        'trip_distance': 5.5
+    }
+
+    actual_features = model_service.prepare_features(ride)
+
+    expected_features = {
+        'PU_DO': '10_20',
+        'trip_distance': 5.5
+    }
+
+    assert actual_features == expected_features
+
+class ModelMock:
+    def __init__(self, value):
+        self.value = value
+
+    def predict(self, X):
+        n = len(X)
+        return [self.value] * n
+
+def test_predict():
+    model_mock = ModelMock(10.0) 
+    model_service = model.ModelService(model_mock)
+
+    features = {
+        'PU_DO': '130_205',
+        'trip_distance': 3.66
+    }
+
+    actual_prediction = model_service.predict(features)
+    expected_prediction = 10.0  
+
+    assert actual_prediction == expected_prediction
+
+def test_lambda_handler():
+    model_mock = ModelMock(10.0)  
+    model_version = "Test123"
+    model_service = model.ModelService(model_mock, model_version)
+
+    event = {
+        "Records": [{
+            "kinesis": {
+                "data": "ewogICAgICAgICJyaWRlIjogewogICAgICAgICAgICAiUFVMb2NhdGlvbklEIjogMTMwLAogICAgICAgICAgICAiRE9Mb2NhdGlvbklEIjogMjA1LAogICAgICAgICAgICAidHJpcF9kaXN0YW5jZSI6IDMuNjYKICAgICAgICB9LCAKICAgICAgICAicmlkZV9pZCI6IDE1NgogICAgfQ=="
+            }
+        }]
+    }
+
+    actual_predictions = model_service.lambda_handler(event)
+
+    expected_predictions = {
+        'predictions': [{
+            'model': 'ride_duration_prediction_model',
+            'version': model_version,
+            'prediction': {
+                'ride_duration': 10,
+                'ride_id': 156
+            }
+        }]
+    }
+
+    assert actual_predictions == expected_predictions
