b589584c9d3ffbfdb103e375958e5b6ae3ae145f
Maksat Yskak <maqsat.ysqaq@gmail.com>
2025-11-07 01:21:29 +0000

    Week 06: 6b.3

---
+++ 06-best-practices/infrastructure/modules/s3/main.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket        = var.bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "s3_bucket" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket]
+
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = "private"
+}
+
+output "name" {
+  value = aws_s3_bucket.s3_bucket.bucket
+}
--- 06-best-practices/infrastructure/variables.tf
+++ 06-best-practices/infrastructure/variables.tf
@@ -11,3 +11,27 @@ variable "project_id" {
 variable "source_stream_name" {
   description = ""
 }
+
+variable "output_stream_name" {
+  description = ""
+}
+
+variable "model_bucket" {
+  description = "s3_bucket"
+}
+
+variable "lambda_function_local_path" {
+  description = ""
+}
+
+variable "docker_image_local_path" {
+  description = ""
+}
+
+variable "ecr_repo_name" {
+  description = ""
+}
+
+variable "lambda_function_name" {
+  description = ""
+}
