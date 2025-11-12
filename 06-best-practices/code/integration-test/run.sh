77fd1b37fe81e713ca4f050d0daf08473188cf4a
Maksat Yskak <maqsat.ysqaq@gmail.com>
2025-11-12 02:51:06 +0000

    fix typo -_-

--- 06-best-practices/code/integration-test/run.sh
+++ 06-best-practices/code/integration-test/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [[ -z "${GITHUB_ACTIONS}" ]]; then
     cd "$(dirname "$0")"
--- 06-best-practices/code/scripts/publish.sh
+++ 06-best-practices/code/scripts/publish.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "publishing image ${LOCAL_IMAGE_NAME} to ECR..."
